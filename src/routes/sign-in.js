57e742cfcf6bda643557dd18caf6398f12693794
Ross Wilson <me@rosswilson.co.uk>
2020-05-03 21:05:54 +0100

    Simplify routes into a single file

--- src/routes/sign-in.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const signInController = require("../controllers/sign-in");
-
-const router = express.Router();
-
-router.get("/", signInController.index);
-router.post("/", signInController.create);
-
-module.exports = router;
